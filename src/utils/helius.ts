44e53a14aeda3fc272ab3a623faba26fa464e841
dannpl <dancardosobr@gmail.com>
2023-08-21 18:15:17 +0100

    add keys

--- src/utils/helius.ts
+++ src/utils/helius.ts
@@ -1,4 +1,4 @@
-const KEYS = process.env.NEXT_APP_API_KEYS?.split(",") || [];
+const KEYS = process.env.NEXT_PUBLIC_API_KEYS?.split(",") || [];
 
 const HELIUS_API = `https://rpc.helius.xyz/?api-key=${
   KEYS[Math.floor(Math.random() * KEYS.length)]
